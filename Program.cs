de19c06029e2b1171d6096bc234af19fbae962c1
idanalex4565 <45922546+idanalex4565@users.noreply.github.com>
2018-12-25 20:56:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+		1)
+			int a,b,c;
+			Console.WriteLine("enter 1st number");
+			a = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("enter 2nd number");
+			b = Convert.ToInt32(Console.ReadLine());
+			c = a + b;
+			Console.Write("the sum is :");
+			Console.WriteLine(c);
+			if (c % 2 == 0) 
+			{
+				Console.WriteLine("even");
+			}
+			else
+			{
+				Console.WriteLine("odd");
+			}
+			Console.WriteLine("Oho");
+			Console.ReadLine();
+		2)
+			int a = Convert.ToInt32(Console.ReadLine());
+			if (a % 10 >= a / 10)
+			{
+				if (a % 10 == a / 10)
+				{
+					Console.WriteLine("shavim");
+				}
+				else
+				{
+					Console.WriteLine("ahadot bigger");
+				}
+			}
+			else
+			{
+				Console.WriteLine("asarot bigger");
+			}
+			Console.ReadLine();
+		3)
+			int a = Convert.ToInt32(Console.ReadLine());
+			if (a > 0)
+			{
+				Console.WriteLine("positive");
+			}
+			else
+			{
+				if (a == 0)
+				{
+					Console.WriteLine("zero");
+				}
+				else
+				{
+					Console.WriteLine("negitive");
+				}
+			}
+			Console.ReadLine();
+		4)
+			Console.WriteLine("enter 1st number");
+			int a = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("enter 2nd number");
+			int b = Convert.ToInt32(Console.ReadLine());
+			if (a > b)
+			{
+				Console.WriteLine("{0} is bigger",a);
+			}
+			else
+			{
+				Console.WriteLine("{0} is bigger",b);
+			}
+			Console.ReadLine();
+		4)
+			int a, i;
+			i = 0;
+			Console.WriteLine("enter a number!");
+			a = Convert.ToInt32(Console.ReadLine());
+			while (a % 2 == 0)
+			{
+				i++;
+				Console.WriteLine("enter a number!");
+				a = Convert.ToInt32(Console.ReadLine());
+			}
+			Console.WriteLine("you entered {0} even numbers",i);
+			Console.ReadLine();
+		5)
+			Console.WriteLine("enter a number");
+			int a = Convert.ToInt32(Console.ReadLine());
+			while (a / 10 != 0)
+			{
+				Console.WriteLine(a % 10);
+				a = a / 10;
+			}
+			Console.WriteLine(a);
+			Console.ReadLine();
+			*/
+		}
+	}
+}
